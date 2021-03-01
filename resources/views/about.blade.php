0bb87099c815a8c3daebbe531582325548d4ad7f
abubakr2234 <68422152+abubakr2234@users.noreply.github.com>
2021-03-01 08:02:22 +0200

    Add files via upload

---
+++ resources/views/about.blade.php
@@ -0,0 +1,26 @@
+@extends('layout.default')
+
+@section('content')
+    <section class="h-screen flex items-center">
+        <div class="container mx-auto">
+            <h1 class="text-white text-4xl mb-6 uppercase font-black">
+                 About my website
+            </h1>
+            <p class="text-white text-lg font-light tracking-wider mb-6">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Veritatis libero ea molestiae quas soluta 
+                voluptate consequuntur nesciunt laborum, accusantium, 
+                fugit quod! Hic earum animi facere blanditiis, temporibus 
+                nihil odit minus minima, obcaecati mollitia impedit dolores accusantium voluptatem consequuntur enim dignissimos voluptate illum!
+                 Molestiae reprehenderit saepe provident debitis. Eius, unde tempora?
+                <br><br>
+                Lorem ipsum dolor sit amet consectetur 
+                adipisicing elit. Nihil voluptatum minus 
+                ut eius fugit impedit quas esse enim et necessitatibus ipsum, 
+                ipsam repudiandae ex sequi incidunt, debitis nemo, ipsa maiores.
+                </p>
+                <a href="/" class="text-purple-500">Go Home</a>
+        </div>
+
+    </section>
+@endsection
---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layout.default')
+
+@section('content')
+    <section class="h-screen flex items-center">
+        <div class="container mx-auto">
+            <h1 class="text-white text-8xl mb-6 uppercase font-black">
+                Abubakr <span class="text-purple-500">Hartley</span>
+            </h1>
+            <h3 class="text-white text-4xl font-light tracking-wider"> wellcome to my website that i created in laravel</h3>
+        </div>
+
+    </section>
+@endsection
